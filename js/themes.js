cf13642bf9739190e2ec7c7c31783a9fb746d771
ebonatto <ebonatto.dev@gmail.com>
2023-05-27 22:10:05 -0300

    Correção título em ingles no responsivo

--- js/themes.js
+++ js/themes.js
@@ -14,28 +14,21 @@ document.querySelector("#theme-checkbox").addEventListener("change", () => {
 apresIntro = document.querySelector(".apresentacao-conteudo-intro");
 apresTitulo = document.querySelector(".apresentacao-conteudo-titulo");
 apresTexto = document.querySelector(".apresentacao-conteudo-texto");
+cvTexto = document.querySelector(".cv-texto");
 
 document.querySelector("#lang-checkbox").addEventListener("change", () => {
   if (apresIntro.innerText === "Olá, meu nome é") {
+    apresTitulo.classList.toggle("eng");
     apresIntro.innerText = "Hi, my name is";
     apresTitulo.innerText = "I am a Jr. Developer";
     apresTexto.innerText =
       "I'm looking for an opportunity to start my career as a programmer in a learning-friendly environment to develop my skills.";
+    cvTexto.innerText = "Resumé";
   } else {
     apresIntro.innerText = "Olá, meu nome é";
     apresTitulo.innerText = "Sou um Dev. Jr";
     apresTexto.innerText =
       "Estou em busca de uma oportunidade para iniciar minha carreira como programador em um ambiente propício para meu aprendizado e desenvolvimento.";
+    cvTexto.innerText = "Currículo";
   }
 });
-
-{
-  /* <p class="apresentacao-conteudo-intro">Olá, meu nome é</p>
-        <h1 class="apresentacao-conteudo-nome">Eduardo Bonatto</h1>
-        <h2 class="apresentacao-conteudo-titulo">Sou um Dev. Jr.</h2>
-        <p class="apresentacao-conteudo-texto">
-          Estou em busca de uma oportunidade para iniciar minha carreira como
-          programador em um ambiente propício para meu aprendizado e
-          desenvolvimento.
-      </p> */
-}
