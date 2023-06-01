75f92deffcccf70085d99a6a47b570a442ac31a2
ebonatto <ebonatto.dev@gmail.com>
2023-06-01 19:42:22 -0300

    Links para itens em ingles

--- js/main.js
+++ js/main.js
@@ -15,6 +15,8 @@ apresIntro = document.querySelector(".apresentacao-conteudo-intro");
 apresTitulo = document.querySelector(".apresentacao-conteudo-titulo");
 apresTexto = document.querySelector(".apresentacao-conteudo-texto");
 cvTexto = document.querySelector(".cv-texto");
+cvLink = document.querySelector(".cv-link");
+linkedinLink = document.querySelector(".linkedin-link");
 
 document.querySelector("#lang-checkbox").addEventListener("change", () => {
   if (apresIntro.innerText === "Olá, meu nome é") {
@@ -24,11 +26,16 @@ document.querySelector("#lang-checkbox").addEventListener("change", () => {
     apresTexto.innerText =
       "I'm looking for an opportunity to start my career as a programmer in a learning-friendly environment to develop my skills.";
     cvTexto.innerText = "Resumé";
+    cvLink.href = "./Resume_EBonatto_EN.pdf";
+    linkedinLink.href =
+      "https://www.linkedin.com/in/ebonattocosta/?locale=en_US";
   } else {
     apresIntro.innerText = "Olá, meu nome é";
     apresTitulo.innerText = "Sou um Dev. Jr";
     apresTexto.innerText =
       "Estou em busca de uma oportunidade para iniciar minha carreira como programador em um ambiente propício para meu aprendizado e desenvolvimento.";
     cvTexto.innerText = "Currículo";
+    cvLink.href = "./CV_EBonatto.pdf";
+    linkedinLink.href = "https://www.linkedin.com/in/ebonattocosta/";
   }
 });
